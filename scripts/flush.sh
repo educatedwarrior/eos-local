63524a456d23f98571a50098ef9944169edc73ac
Gabo Esquivel <contact@gaboesquivel.com>
2019-01-05 14:44:19 -0600

    build(scripts): don't remove wallet docker volume when flushing

--- scripts/flush.sh
+++ scripts/flush.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "Flushing all blockchain and databases data"
+echo "Flushing all blockchain and database data"
 docker-compose down -v
 
-# remove o
-rm "$(dirname "$0")/../services/eosio/config/keys/eoslocal_wallet_password.txt"
+echo "=======================IMPORTANT================================"
+echo "The wallet docker volume and the eoslocal wallet password file are not removed by this command."
+echo "That is in order avoid losing keys."
+echo "================================================================"
